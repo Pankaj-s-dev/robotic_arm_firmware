eaf2f55858209f5edca07082d525d78a29e99154
mowito <pankaj.kumar@mowito.in>
2023-10-20 18:57:52 +0530

    pk: changing directory

---
+++ .pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.o: \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ .pio/build/esp32dev/FrameworkArduino/stdlib_noniso.c.d
@@ -0,0 +1,12 @@
+.pio/build/esp32dev/FrameworkArduino/stdlib_noniso.c.o: \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.c \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ /home/mowito/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h
